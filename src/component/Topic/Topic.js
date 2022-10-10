f4a0529ebcb076ea8d6e0c8f5a21a6e91f7d3d04
abubakkar11 <abubakkar995@gmail.com>
2022-10-10 14:16:12 -0700

    start quice added

--- src/component/Topic/Topic.js
+++ src/component/Topic/Topic.js
@@ -1,19 +1,18 @@
 import React from 'react';
-import { BeakerIcon } from '@heroicons/react/24/solid'
+import { ArrowSmallRightIcon } from '@heroicons/react/24/solid'
 
 const Topic = ({topic}) => {
     const {logo , name} = topic;
     return (
-        <div className='w-52 items-center bg-white drop-shadow-md inline p-2'>
+        <div className='w-56 items-center bg-slate-200 drop-shadow-md inline p-2'>
             <div>
-               <img className='w-48 ' src={logo} alt="" />
+               <img className='w-52 ' src={logo} alt="" />
             </div>
             <div className='flex mt-3 justify-between'>
                 <h3 className='mr-5'>{name}</h3>
-                <button className='bg-blue-600 p-1 text-white hover:bg-blue-400'>
-                    
-                 Start Quice
-                 <BeakerIcon className="h-6 w-6 text-blue-500"/>
+                <button className='bg-blue-600 p-1 text-white hover:bg-blue-400 rounded-sm	 flex items-center'>
+                Start Quice
+                <ArrowSmallRightIcon className='w-5'/>
                 </button>
             </div>
         </div>
