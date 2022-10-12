58b90a4d331edf9518232f2fcbe4d8e0c0f2f35d
abubakkar11 <abubakkar995@gmail.com>
2022-10-11 22:28:36 -0700

    Corrects Answer Added

--- src/Quiz/Quiz.js
+++ src/Quiz/Quiz.js
@@ -7,10 +7,14 @@ import 'react-toastify/dist/ReactToastify.css';
 const Quiz = ({quiz}) => {
     const [anser , setAnswer] = useState([]);
     const {question , options , correctAnswer} = quiz;
-    console.log(quiz)
     const notify = () => toast(`${correctAnswer}`);
-    const quizAnser = () =>{
-
+    const quizAnser = (answer) =>{
+      if( answer === correctAnswer){
+       toast('Wrong Answer');
+      }
+      else{
+      toast('Correct Answer');
+      }
     }
     return (
    
@@ -22,12 +26,12 @@ const Quiz = ({quiz}) => {
             <button onClick={notify}><EyeIcon className="h-6 w-10 ml-96 text-blue-500"/></button>
             <ToastContainer />
             </div>
-            <div className='grid md:grid-cols-2 gap-5'>
+            <div className='grid md:grid-cols-2 gap-5 '>
                {
                 options.map(option => <QuizOption
                 option = {option}
                 quizAnser = {quizAnser}
-                
+                setAnswer = {setAnswer}
                 ></QuizOption>)
                }
             </div>
--- src/component/QuizOption/QuizOption.js
+++ src/component/QuizOption/QuizOption.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const QuizOption = ({option , quizAnser}) => {
+const QuizOption = ({option , quizAnser ,setAnswer}) => {
     return (
         <div className='hover:bg-slate-500 hover:text-white hover:rounded-md '>
-          <button onClick={quizAnser} className='p-5 px-44 w-full  flex items-center border border-2 border-gray-500 rounded-md'>
+          <button onClick={() => quizAnser(option)} className='p-5 px-44 w-full  flex items-center border border-2 border-gray-500 rounded-md'>
             {option}
           </button>
         </div>
